a1f586667ebbf0538a1a05a52e2cbb400ed1c446
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2022-12-13 15:25:09 +0100

    primo esercizio

---
+++ snak-1/script.js
@@ -0,0 +1,18 @@
+/*Il software deve chiedere per 10 volte all’utente di inserire un numero.
+ (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.*/
+
+
+//creo una variabile per la somma e seleziono il target dove stampare il risultato
+let sommatoria = 0;
+const target = document.getElementById("contenitore");
+//eseguo 10 volte un prompt grazie al for
+for(i=1 ; i <= 3 ; i++){
+
+    let val = parseInt(prompt("inserisci il numero"));
+    console.log(val)
+    sommatoria += val;
+
+    if(i === 3){
+        target.innerText = `la somma totale è : ${sommatoria}`
+    }
+}
